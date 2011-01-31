3f715a2301dad37f95b46376fcf7b22c3b9c47c2
Michael Savage <mikejsavage@gmail.com>
2011-01-31 20:17:05 +0000

    put the - back in weapon trees when a weapon has no element

--- templates/weapons/meleeTreeItem.lua
+++ templates/weapons/meleeTreeItem.lua
@@ -2,7 +2,7 @@
 	<td>{{ ( "&nbsp;&nbsp;&nbsp;" ):rep( depth ) }}<a class="rare{{ weapon.rarity }}{{ weapon.create and " create" or "" }}" href="{{ U( ( "weapons/%s/%s" ):format( class.short, urlFromName( weapon.name ) ) ) }}">{{ weapon.name.hgg }}</a></td>
 	<td>{{ weapon.attack }}</td>
 	<td>?</td>
-	{{ weapon.element and ( "<td class='elem%s'>%d" ):format( weapon.element, weapon.elemAttack ) or "<td>" }}</td>
+	{{ weapon.element and ( "<td class='elem%s'>%d" ):format( weapon.element, weapon.elemAttack ) or "<td>-" }}</td>
 	<td{{ weapon.affinity ~= 0 and ( ( " class='%s'" ):format( weapon.affinity > 0 and "pos" or "neg" ) ) or ""}}>{{ weapon.affinity }}%</td>
 	<td>{{ weapon.sharpness and sharpness( { sharpness = weapon.sharpness } ) or "?" }}</td>
 	<td>{{ weapon.slots == 0 and "-" or ( "O" ):rep( weapon.slots ) }}</td>
