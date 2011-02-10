46d957587fba628a72771d746cc8b92fadac5565
Michael Savage <mikejsavage@gmail.com>
2011-02-10 17:01:52 +0000

    added rarities to item pages

--- templates/items/itemInfo.lua
+++ templates/items/itemInfo.lua
@@ -1,4 +1,4 @@
 <h1>{{ itemName( { item = item } ) }}</h1>
 
 ID: {{ item.id }} (0x{{ ( "%04x" ):format( item.id ):upper() }})<br>
-{% --Rarity: <span class="rare{{ item.rarity }}">{{ item.rarity }}</span> %}
+Rarity: <span class="rare{{ item.rarity }}">{{ item.rarity or "?" }}</span>
