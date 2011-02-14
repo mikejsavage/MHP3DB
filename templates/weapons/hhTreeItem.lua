e2986d86fc3f262df1199e586d2a91897792e180
Michael Savage <mikejsavage@gmail.com>
2011-02-14 22:07:09 +0000

    fixed tree items always using hgg translations

--- templates/weapons/hhTreeItem.lua
+++ templates/weapons/hhTreeItem.lua
@@ -1,5 +1,5 @@
 {{ depth == 0 and "<tr class='split'>" or "<tr>" }}
-	<td>{{ ( "&nbsp;&nbsp;&nbsp;" ):rep( depth ) }}<a class="rare{{ weapon.rarity }}{{ weapon.create and " create" or "" }}" href="{{ U( ( "weapons/%s/%s" ):format( class.short, urlFromName( weapon.name ) ) ) }}">{{ weapon.name.hgg }}</a></td>
+	<td>{{ ( "&nbsp;&nbsp;&nbsp;" ):rep( depth ) }}<a class="rare{{ weapon.rarity }}{{ weapon.create and " create" or "" }}" href="{{ U( ( "weapons/%s/%s" ):format( class.short, urlFromName( weapon.name ) ) ) }}">{{ T( weapon.name ) }}</a></td>
 	<td>{{ weapon.attack }}</td>
 	<td>{{ getTATP( weapon ) }}</td>
 
