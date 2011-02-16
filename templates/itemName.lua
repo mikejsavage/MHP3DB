e9d622398296af3ed9c616f1c97d1191d0057104
Michael Savage <mikejsavage@gmail.com>
2011-02-16 19:48:14 +0000

    fixed default item icon

--- templates/itemName.lua
+++ templates/itemName.lua
@@ -1,1 +1,1 @@
-{{ icon( "items/" .. ( item.icon and item.icon or "garbage" ), item.color or "ffffff" ) }} {{ T( item.name ) }}
+{{ icon( "items/" .. ( item.icon and item.icon or "garbage" ), item.color or "white" ) }} {{ T( item.name ) }}
