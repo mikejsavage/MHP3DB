1eff2dd5198f4a15aec2749b6570ad96c92a586f
Michael Savage <mikejsavage@gmail.com>
2011-02-25 12:52:03 +0000

    removed useless comment

--- templates/weapons/meleeTreeItem.lua
+++ templates/weapons/meleeTreeItem.lua
@@ -10,28 +10,6 @@ local TreeInfos =
 
 local treeInfo = loadTemplate( TreeInfos[ class.short ] or TreeInfos.default )
 
---[[
--- why did i bother...
--- HAY WHY NOT CACHE THIS TOO???????
-
-
-local path = "["
-
-if weapon.path then
-	local pathLength = table.getn( weapon.path )
-
-	if pathLength ~= 0 then
-		for i = 1, pathLength - 1 do
-			path = ( "%s%d," ):format( path, weapon.path[ i ] )
-		end
-
-		path = ( "%s%d" ):format( path, weapon.path[ pathLength ] )
-	end
-end
-
-path = path .. "]"
-]]
-
 local path = weapon.path and json.encode( weapon.path ) or "[]"
 %}
 
