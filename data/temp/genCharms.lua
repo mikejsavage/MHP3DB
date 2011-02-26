85c70a5a1bbc712444f305c7ae7b6d1910633203
Michael Savage <mikejsavage@gmail.com>
2011-02-26 14:00:14 +0000

    genCharms is now functional, albeit slow

--- data/temp/genCharms.lua
+++ data/temp/genCharms.lua
@@ -4,16 +4,16 @@ require( "common" )
 
 local DataPath = "charms/charms.csv"
 
+local Classes =
+{
+	[ "Queen Talisman" ] = "queen",
+	[ "King Talisman" ] = "king",
+	[ "Dragon Talisman" ] = "dragon",
+}
+
 Skills = data( "skills" )
 
 local Charms = { }
-
-local Unused = { }
-local Bad = { }
-
-for _, skill in ipairs( Skills ) do
-	Unused[ skill.name.hgg ] = true
-end
 
 io.input( DataPath )
 
@@ -21,56 +21,78 @@ io.input( DataPath )
 
 for line in io.lines() do
 	if line ~= "ID,Charm Type,Charm Name,Skill 1,Skill 2,Charm Slot,Table No.\r" then
-		local success, _, skill1, skill2, slots, table =
-			line:find( "^%d+,[%a ]+,[%a ]+,([%a%+%-/%d ]*),([%a%+%-/%d ]*),(%d+),([%a%d]+)\r$" )
+		local success, _, class, skill1, skill2, slots, charmTable =
+			line:find( "^%d+,[%a ]+,([%a ]+),([%a%+%-/%d ]*),([%a%+%-/%d ]*),(%d+),(%d+)\r?$" )
+
+		local charm =
+		{
+			type = assert( Classes[ class ], "bad type: " .. class ),
+			skills = { },
+			slots = tonumber( slots ),
+			table = tonumber( charmTable ),
+		}
 
 		assert( success, "bad line: " .. line )
 
 		local id1, points1 = parseSkill( skill1 )
 
-		if not id1 then
-			local _, _, name = skill1:find( "^([%a%-/ ]+) [%+%-]?%d+$" )
+		assert( id1, "bad skill1: " .. skill1 )
 
-			assert( name, "what " .. skill1 )
-
-			Bad[ name ] = true
-		else
-			local _, _, name = skill1:find( "^([%a%-/ ]+) [%+%-]?%d+$" )
-
-			Unused[ name ] = nil
-		end
-
-		--assert( id1, "bad skill1 in " .. line )
+		table.insert( charm.skills, { id = id1, points = points1 } )
 
 		if skill2 ~= "" then
 			local id2, points2 = parseSkill( skill2 )
 
-			if not id2 then
-				local _, _, name = skill2:find( "^([%a%-/ ]+) [%+%-]?%d+$" )
+			assert( id2, "bad skill2: " .. skill2 )
 
-				assert( name, "what " .. skill2 )
-
-				Bad[ name ] = true
-			else
-				local _, _, name = skill2:find( "^([%a%-/ ]+) [%+%-]?%d+$" )
-
-				Unused[ name ] = nil
-			end
-
-			--assert( id2, "bad skill1 in " .. line )
+			table.insert( charm.skills, { id = id2, points = points2 } )
 		end
+
+		table.insert( Charms, charm )
 	end
 end
 
-for name, _ in pairs( Bad ) do
-	print( "bad: " .. name )
+-- there's so many charms that everything needs to be done by hand
+-- and we can't put it all in a string because my pc doesn't have
+-- enough memory
+
+function skillsJSON( skills )
+	local len = table.getn( skills )
+
+	if len == 1 then
+		return ( [[{"id":%d,"points":%d}]] ):format(
+			skills[ 1 ].id, skills[ 1 ].points
+		)
+	elseif len == 2 then
+		return ( [[{"id":%d,"points":%d},{"id":%d,"points":%d}]] ):format(
+			skills[ 1 ].id, skills[ 1 ].points,
+			skills[ 2 ].id, skills[ 2 ].points
+		)
+	end
+
+	assert( nil, "bad len" )
 end
 
-for name, _ in pairs( Unused ) do
-	print( "unused: " .. name )
+io.output( "../charms.json" )
+
+io.write( "[" )
+
+local lenmm = table.getn( Charms ) - 1 -- len--
+
+for i = 1, lenmm do
+	local charm = Charms[ i ]
+
+	local charmText = ( [[{"type":"%s","table":%d,"slots":%d,"skills":[%s]},]] ):format(
+		charm.type, charm.table, charm.slots, skillsJSON( charm.skills )
+	)
+
+	io.write( charmText )
 end
 
-print( "genCharms: ok!" )
+local charm = Charms[ lenmm + 1 ]
 
---io.output( "../charms.json" )
---io.write( json.encode( Charms ) )
+local charmText = ( [=[{"type":"%s","table":%d,"slots":%d,"skills":[%s]}]]=] ):format(
+	charm.type, charm.table, charm.slots, skillsJSON( charm.skills )
+)
+
+io.write( charmText )
