20fdc09fa2277d18f56e640a6c620b049c921cdd
Michael Savage <mikejsavage@gmail.com>
2011-03-20 14:39:48 +0000

    i forgot to add the icon template in an earlier commit...

--- templates/icon.lua
+++ templates/icon.lua
@@ -1,1 +1,6 @@
-<img src="{{ U( "data/img/icons/" .. ( "%s_%s.png" ):format( icon, color ) ) }}" class="icon">
+<img src="{{ U( "data/img/icons/" ..
+	( color
+		and ( "%s_%s.png" ):format( icon, color )
+		or  ( "%s.png" ):format( icon ) )
+	)
+}}" class="icon">
