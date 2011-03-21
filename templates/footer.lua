40cfeffea461555751ad490613ba78f080d6975f
Michael Savage <mikejsavage@gmail.com>
2011-03-21 18:33:01 +0000

    added link to live page to footer when viewing locally

--- templates/footer.lua
+++ templates/footer.lua
@@ -1,6 +1,12 @@
 			</div>
 
 			<div class="footer">
+				{%
+					if IsLocalHost then
+						printf( [[<a href="http://divinewh.im/p3/%s">Live link</a> / ]], CurrentUrl )
+					end
+				%}
+
 				All data is property of Capcom / <a href="http://divinewh.im/f/">Leave feedback</a> / <a href="https://github.com/mikejsavage/MHP3DB">Mike Savage</a>
 			</div>
 		</div>
