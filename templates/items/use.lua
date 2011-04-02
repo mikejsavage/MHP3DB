330c915da7e4f852756e6242da177614625b87bf
Michael Savage <mikejsavage@gmail.com>
2011-04-02 12:55:45 +0100

    tidied item uses template

---
+++ templates/items/use.lua
@@ -0,0 +1,5 @@
+<tr>
+	<td>{{ type }}</td>
+	<td class="useObject">{{ name }}</td>
+	<td>{{ count }}</td>
+</tr>
--- templates/items/uses.lua
+++ templates/items/uses.lua
@@ -1,63 +1,73 @@
+{%
+	local use = loadTemplate( "items/use" )
+%}
+
 <h2>Used for</h2>
 
 <table class="data">
-	<tr>
-		<th>Use</th>
-		<th>Object</th>
-		<th>Req</th>
-	</tr>
+	<thead>
+		<tr>
+			<th>Use</th>
+			<th>Object</th>
+			<th>Req</th>
+		</tr>
+	</thead>
 
-	{%
-		if item.uses.weapons then
-			local weaponNameURL = loadTemplate( "weapons/weaponNameURL" )
+	<tbody>
+		{%
+			if item.uses.weapons then
+				local weaponNameURL = loadTemplate( "weapons/weaponNameURL" )
 
-			for _, weaponUse in ipairs( item.uses.weapons ) do
-				local class = weaponClassFromShort( weaponUse.short )
+				for _, weaponUse in ipairs( item.uses.weapons ) do
+					local class = weaponClassFromShort( weaponUse.short )
 
-				printf( "<tr><td>%s</td><td>%s</td><td>%d</td></tr>",
-					weaponUse.type == "create" and "Creating" or "Upgrading to",
-					weaponNameURL( { class = class, weapon = class.weapons[ weaponUse.id ] } ),
-					weaponUse.count
-				)
+					print( use( {
+						type = weaponUse.type == "create" and "Creating" or "Upgrading to",
+						name = weaponNameURL( { class = class, weapon = class.weapons[ weaponUse.id ] } ),
+						count = weaponUse.count
+					} ) )
+				end
 			end
-		end
 
-		if item.uses.guns then
-			local weaponNameURL = loadTemplate( "weapons/weaponNameURL" )
+			if item.uses.guns then
+				local weaponNameURL = loadTemplate( "weapons/weaponNameURL" )
 
-			for _, gunUse in ipairs( item.uses.guns ) do
-				local class = gunClassFromShort( gunUse.short )
+				for _, gunUse in ipairs( item.uses.guns ) do
+					local class = gunClassFromShort( gunUse.short )
 
-				printf( "<tr><td>%s</td><td>%s</td><td>%d</td></tr>",
-					gunUse.type == "create" and "Creating" or "Upgrading to",
-					weaponNameURL( { class = class, weapon = class.weapons[ gunUse.id ] } ),
-					gunUse.count
-				)
+					print( use( {
+						type = gunUse.type == "create" and "Creating" or "Upgrading to",
+						name = weaponNameURL( { class = class, weapon = class.weapons[ gunUse.id ] } ),
+						count = gunUse.count
+					} ) )
+				end
 			end
-		end
 
-		if item.uses.armors then
-			local pieceNameURL = loadTemplate( "armory/pieceNameURL" )
+			if item.uses.armors then
+				local pieceNameURL = loadTemplate( "armory/pieceNameURL" )
 
-			for _, armorUse in ipairs( item.uses.armors ) do
-				local class = armorClassFromShort( armorUse.short )
+				for _, armorUse in ipairs( item.uses.armors ) do
+					local class = armorClassFromShort( armorUse.short )
 
-				printf( "<tr><td>Creating</td><td>%s</td><td>%d</td></tr>",
-					pieceNameURL( { class = class, piece = class.pieces[ armorUse.id ] } ),
-					armorUse.count
-				)
+					print( use( {
+						type = "Creating",
+						name = pieceNameURL( { class = class, piece = class.pieces[ armorUse.id ] } ),
+						count = armorUse.count
+					} ) )
+				end
 			end
-		end
 
-		if item.uses.decorations then
-			local decorNameURL = loadTemplate( "armory/decorNameSlotsURL" )
+			if item.uses.decorations then
+				local decorNameSlotsURL = loadTemplate( "armory/decorNameSlotsURL" )
 
-			for _, decorUse in ipairs( item.uses.decorations ) do
-				printf( "<tr><td>Creating</td><td>%s</td><td>%d</td></tr>",
-					decorNameURL( { decor = Decorations[ decorUse.id ] } ),
-					decorUse.count
-				)
+				for _, decorUse in ipairs( item.uses.decorations ) do
+					print( use( {
+						type = "Creating",
+						name = decorNameSlotsURL( { decor = Decorations[ decorUse.id ] } ),
+						count = decorUse.count
+					} ) )
+				end
 			end
-		end
-	%}
+		%}
+	</tbody>
 </table>
